2a5b7af6dbb5ff1583fe4fa7933df1a18cd81df4
nadirdeveloper <63139854+nadirdeveloper@users.noreply.github.com>
2020-12-08 21:42:25 +0500

    Updates

---
+++ precache-manifest.9b17f309d0cc66a6a3297a685a1042fd.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f73ce611f4946d4e910f6ec7514a80",
+    "url": "/weather-app-test/index.html"
+  },
+  {
+    "revision": "15838a9af7feff73e3b9",
+    "url": "/weather-app-test/static/css/1.ebc88654.chunk.css"
+  },
+  {
+    "revision": "33d5ca17ea2fc78af78a",
+    "url": "/weather-app-test/static/css/6.fbea9171.chunk.css"
+  },
+  {
+    "revision": "89cee7000ee927d68303",
+    "url": "/weather-app-test/static/css/main.5f3ce2f1.chunk.css"
+  },
+  {
+    "revision": "53eb3e180dacf596ba15",
+    "url": "/weather-app-test/static/js/0.65985b9c.chunk.js"
+  },
+  {
+    "revision": "97636669c8193ace9a3b4f1548fc4d0b",
+    "url": "/weather-app-test/static/js/0.65985b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15838a9af7feff73e3b9",
+    "url": "/weather-app-test/static/js/1.42937a52.chunk.js"
+  },
+  {
+    "revision": "aea5fd4b1e1913f0606e",
+    "url": "/weather-app-test/static/js/10.1171d154.chunk.js"
+  },
+  {
+    "revision": "06e1f4346f07aecd8ed8",
+    "url": "/weather-app-test/static/js/4.ef9eb659.chunk.js"
+  },
+  {
+    "revision": "6d3ee6e37fe58a43e0c3af40c9e0e4ce",
+    "url": "/weather-app-test/static/js/4.ef9eb659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36df674342e3b5490c05",
+    "url": "/weather-app-test/static/js/5.c24b6e76.chunk.js"
+  },
+  {
+    "revision": "33d5ca17ea2fc78af78a",
+    "url": "/weather-app-test/static/js/6.c5b22bd3.chunk.js"
+  },
+  {
+    "revision": "edb614846928bf9e3347",
+    "url": "/weather-app-test/static/js/7.91beb0e3.chunk.js"
+  },
+  {
+    "revision": "176fa11adff3f658e505",
+    "url": "/weather-app-test/static/js/8.43b56f80.chunk.js"
+  },
+  {
+    "revision": "a976a31e9579f7a498cf",
+    "url": "/weather-app-test/static/js/9.2af70c40.chunk.js"
+  },
+  {
+    "revision": "89cee7000ee927d68303",
+    "url": "/weather-app-test/static/js/main.82394028.chunk.js"
+  },
+  {
+    "revision": "35b9c846d5dfbd966623",
+    "url": "/weather-app-test/static/js/runtime-main.c9badae8.js"
+  },
+  {
+    "revision": "788e7c705c377d9e08875341f0e860cb",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Bold.788e7c70.woff2"
+  },
+  {
+    "revision": "7b9e40f6db710fa59344e5dc8cc34539",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Bold.7b9e40f6.woff"
+  },
+  {
+    "revision": "ff7cc358afba9c9f9dbfe590da86fdc4",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Bold.ff7cc358.ttf"
+  },
+  {
+    "revision": "3564bf8ea8fd95ef7fc035ccb3dcc6bd",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Light.3564bf8e.ttf"
+  },
+  {
+    "revision": "3a844470ad64cd09f292369c7841ac9a",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Light.3a844470.woff2"
+  },
+  {
+    "revision": "f5c67c0c4c54897c8b64ac0957fdc364",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Light.f5c67c0c.woff"
+  },
+  {
+    "revision": "3d44559f46b9069844ce17bcf967f264",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Medium.3d44559f.ttf"
+  },
+  {
+    "revision": "61a3f6134b0f3a5eb11b6b740ffcabaa",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Medium.61a3f613.woff"
+  },
+  {
+    "revision": "ed1a889bb737857a86acd44abfc1a9f4",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Medium.ed1a889b.woff2"
+  },
+  {
+    "revision": "5529b729a77573fc6130f2db44a19c77",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Regular.5529b729.woff2"
+  },
+  {
+    "revision": "982323eb635ecc1f9fc8d20e6bbd9316",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Regular.982323eb.ttf"
+  },
+  {
+    "revision": "fbbf6d98bfb9f3410a0de34728e9250e",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-Regular.fbbf6d98.woff"
+  },
+  {
+    "revision": "0afbf8e46c88529a3f140bacd48f5949",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-SemiBold.0afbf8e4.woff2"
+  },
+  {
+    "revision": "18970edf2f9900dc6928cdc0b64c348b",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-SemiBold.18970edf.ttf"
+  },
+  {
+    "revision": "7b228459800eddb3e86861952daa4528",
+    "url": "/weather-app-test/static/media/SpaceGrotesk-SemiBold.7b228459.woff"
+  }
+]);
